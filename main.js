f3008aa835786049753b7b6744de8da8bd9cb1d5
Khiem Pham <khiempham841@gmail.com>
2022-05-12 19:20:25 -0700

    added image modal

--- main.js
+++ main.js
@@ -21,6 +21,36 @@ document.onscroll = function() {
 }
 
 
+
+// Modal 
+// create references to the modal...
+let modal = document.getElementById('myModal');
+// to all images -- note I'm using a class!
+let images = document.getElementsByClassName('cs-pic');
+// the image in the modal
+let modalImg = document.getElementById("img01");
+// and the caption in the modal
+let captionText = document.getElementById("caption");
+
+// Go through all of the images with our custom class
+for (let i = 0; i < images.length; i++) {
+  let img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function(evt) {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+    modalCheck = true;
+  }
+}
+
+let span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
 // SCROLL UP BUTTON
 let mybutton = document.getElementById('myBtn')
 
